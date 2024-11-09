321e9d52cd6f37d7b78130a9b94b243897b59a18
uthuyein <uthuyeinlinn@gmail.com>
2024-11-09 09:55:56 +0630

    cateria test

---
+++ s12-spring-boot-with-example-and-cateria/ep03-specificationApi/src/main/java/com/jdc/mkt/entity/Region.java
@@ -0,0 +1,31 @@
+package com.jdc.mkt.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "region_tbl")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Region implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	@OneToMany(mappedBy = "region")
+	private List<State> states;
+}
---
+++ s12-spring-boot-with-example-and-cateria/ep03-specificationApi/src/test/java/com/jdc/mkt/test/CateriaTest.java
@@ -0,0 +1,11 @@
+package com.jdc.mkt.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CateriaTest {
+
+	@Test
+	void test() {}
+}
